32a3b148cdb1416d95147dd09d73a339081977f5
Mogeko <zhengjunyi@live.com>
2021-05-29 05:33:31 +0800

    feat: add two links to subtitles

--- Douban2RARBG.user.js
+++ Douban2RARBG.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Douban2RARBG
 // @namespace    https://mogeko.me
-// @version      0.1
+// @version      0.2
 // @description  Add direct links to RARBG & TPB from Douban.
 // @author       Mogeko
 // @match        https://movie.douban.com/subject/*
@@ -11,28 +11,31 @@
 // @grant        none
 // ==/UserScript==
 
-const SITE_DATA = [
-    ["RARBG", "https://rarbgmirror.com/torrents.php?imdb="],
-    ["TPB", "https://thepiratebay.org/search.php?q="]
+const RESOURCE_SITE_DATA = [
+    ["RARBG", "https://rarbgmirror.com/torrents.php?imdb=%i"],
+    ["TPB", "https://thepiratebay.org/search.php?q=%i"]
 ];
 
-(function() {
-    'use strict';
-    const infoNode = document.querySelector("#info");
+const SUBTITLE_SITE_DATA = [
+    ["opensubtitles", "https://www.opensubtitles.org/zh/search/imdbid-%x/sublanguageid-all/moviename-%i"],
+    ["SubHD", "https://subhd.tv/search/%d"]
+];
+
+const getItemNode = (l_Name, siteData, imdb, doubanID) => {
     const itemNode = document.createElement("span");
     const l_Node = document.createElement("span");
     const r_Node = document.createElement("span");
-    const br = document.createElement("br");
-
-    const imdb = infoNode.innerHTML.match(/tt[0-9]{4,}/)[0];
 
     l_Node.className = "pl";
-    l_Node.innerHTML = "资源: ";
+    l_Node.innerHTML = l_Name;
 
-    SITE_DATA.map(site => {
+    siteData.map(site => {
         const link = document.createElement("a");
         link.textContent = site[0];
-        link.href = site[1] + imdb;
+        link.href = site[1]
+            .replace("%i", imdb)
+            .replace("%d", doubanID)
+            .replace("%x", imdb.substring(2));
         link.target="_blank";
         return link;
     }).forEach((node, index, array) => {
@@ -44,6 +47,22 @@ const SITE_DATA = [
 
     itemNode.appendChild(l_Node);
     itemNode.appendChild(r_Node);
-    infoNode.appendChild(itemNode);
-    infoNode.appendChild(br);
+    itemNode.innerHTML += "</br>";
+
+    return itemNode;
+}
+
+(function() {
+    'use strict';
+    const infoNode = document.querySelector("#info");
+
+    const imdb = infoNode.innerHTML.match(/tt[0-9]{4,}/)[0];
+    const doubanID = document.location.toString().split("/")[4];
+
+    infoNode.appendChild(
+        getItemNode("资源: ", RESOURCE_SITE_DATA, imdb)
+    );
+    infoNode.appendChild(
+        getItemNode("字幕: ", SUBTITLE_SITE_DATA, imdb, doubanID)
+    );
 })();
